59b9bb58170997657ed197760fbfd5a9049b20e8
NiladriHazra <niladrivit@gmail.com>
2023-03-04 00:00:00 +0000

    fix warnings in codebase

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -211,4 +211,5 @@ document.addEventListener('click', () => {
 // Handle window resize to keep the canvas responsive
 window.addEventListener('resize', () => {
     if (isPlaying) {
-        // Redraw the visualizer if a song is playing
+        // Redraw the visualizer if a song is playing
+        const bufferLength = analyser.frequencyBinCount;
