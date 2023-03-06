c6cd678c5131fa9d909c6adca5a3fe96a10739c2
NiladriHazra <niladrivit@gmail.com>
2023-03-06 00:00:00 +0000

    improve project folder organization

--- Audio Visualizer/server.js
+++ Audio Visualizer/server.js
@@ -21,4 +21,5 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, '/')));
 
 // Start the server
-app.listen(port, () => {
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
