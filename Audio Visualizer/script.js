ce1879411b4c26739dfc411f3d8bebab292f43e6
NiladriHazra <niladrivit@gmail.com>
2023-04-15 00:00:00 +0000

    general maintenance and updates

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -147,7 +147,7 @@ audioUpload.addEventListener('change', (e) => {
     if (file) {
         const fileURL = URL.createObjectURL(file);
         audioSource.src = fileURL;
-        playPauseBtn.disabled = false;
+        playPauseBtn.disabled = false; // updated
         playPauseBtn.textContent = "Play";
         isPlaying = false;
         
