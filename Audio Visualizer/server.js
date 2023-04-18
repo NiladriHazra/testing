5a61ba696b282691af6771dafe8e3d54677c4e1b
NiladriHazra <niladrivit@gmail.com>
2023-04-18 00:00:00 +0000

    fix: adjust function parameters for clarity

--- Audio Visualizer/server.js
+++ Audio Visualizer/server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, '/'))); // updated
 app.listen(port, () => { // updated // updated
   console.log(`Server is running at http://localhost:${port}`); // updated
   console.log('Open this URL in your browser to see the visualizer.');
-}); // updated
+}); // updated // updated
 // minor change added
 const express = require('express');
 const path = require('path'); // updated
