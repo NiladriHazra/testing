41b55cba156a2cc10c29de0870ae41ffaca4cf61
NiladriHazra <niladrivit@gmail.com>
2023-05-27 00:00:00 +0000

    refactor: minor code structure improvements

--- Audio Visualizer/server.js
+++ Audio Visualizer/server.js
@@ -1,6 +1,6 @@
 const express = require('express'); // updated // updated // updated // updated
 const path = require('path'); // updated // updated
-const app = express(); // updated // updated // updated // updated
+const app = express(); // updated // updated // updated // updated // updated
 const port = 3000; // updated // updated
 // minor change added // updated // updated
 // Serve static files from the current directory // updated
