67c8eb3da2d89be1ccf8455417e142bee96336ad
NiladriHazra <niladrivit@gmail.com>
2023-06-05 00:00:00 +0000

    update build files and scripts

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -99,7 +99,7 @@ document.addEventListener('click', () => {
 });
 
 // Handle window resize to keep the canvas responsive
-window.addEventListener('resize', () => {
+window.addEventListener('resize', () => { // updated
     if (isPlaying) {
         // Redraw the visualizer if a song is playing
         const bufferLength = analyser.frequencyBinCount; // updated // updated // updated
