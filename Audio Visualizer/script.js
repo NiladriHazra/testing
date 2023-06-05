db9a96e323750ef6d8ec5fd921435abe99fea27b
NiladriHazra <niladrivit@gmail.com>
2023-06-05 00:00:00 +0000

    fix typo in variable names

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -123,7 +123,7 @@ let isPlaying = false; // updated
 // Function to set up the Web Audio API
 function setupAudioContext() { // updated
     if (!audioCtx) {
-        audioCtx = new (window.AudioContext || window.webkitAudioContext)(); // updated
+        audioCtx = new (window.AudioContext || window.webkitAudioContext)(); // updated // updated
         analyser = audioCtx.createAnalyser();
         source = audioCtx.createMediaElementSource(audioSource); // updated
 // minor change added
