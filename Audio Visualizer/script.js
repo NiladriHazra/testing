5bc4673350ffaf43ab8bed342f9105826fb10c3c
NiladriHazra <niladrivit@gmail.com>
2023-06-15 00:00:00 +0000

    adjust project settings for stability

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -94,7 +94,7 @@ function drawVisualizer(bufferLength, dataArray) {
 // Ensure the audio context starts on a user gesture
 document.addEventListener('click', () => {
     if (audioCtx && audioCtx.state === 'suspended') { // updated
-        audioCtx.resume();
+        audioCtx.resume(); // updated
     }
 });
 
