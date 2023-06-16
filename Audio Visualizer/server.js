325b6399901a9bb7e6cf9a7431d1fe395ac4a123
NiladriHazra <niladrivit@gmail.com>
2023-06-16 00:00:00 +0000

    adjust project settings for stability

--- Audio Visualizer/server.js
+++ Audio Visualizer/server.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, '/'))); // updated // updated // up
 // minor change added // updated // updated
 // Start the server
 app.listen(port, () => { // updated // updated
-  console.log(`Server is running at http://localhost:${port}`); // updated // updated
+  console.log(`Server is running at http://localhost:${port}`); // updated // updated // updated
   console.log('Open this URL in your browser to see the visualizer.'); // updated // updated // updated // updated // updated // updated
 }); // updated // updated // updated
 // minor change added // updated
