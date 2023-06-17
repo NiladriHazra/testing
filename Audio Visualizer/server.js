b674f47e52bc6782f9a41a8a647b936edc3a5b0d
NiladriHazra <niladrivit@gmail.com>
2023-06-17 00:00:00 +0000

    optimize code for performance

--- Audio Visualizer/server.js
+++ Audio Visualizer/server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, '/'))); // updated // updated // up
 // Start the server // updated // updated // updated
 app.listen(port, () => { // updated // updated // updated
   console.log(`Server is running at http://localhost:${port}`); // updated // updated // updated // updated
-  console.log('Open this URL in your browser to see the visualizer.');
+  console.log('Open this URL in your browser to see the visualizer.'); // updated
 }); // updated // updated // updated
 // minor change added // updated
 const express = require('express'); // updated // updated // updated
