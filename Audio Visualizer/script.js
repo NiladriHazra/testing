f0cee8bb4cf2dbd8c3f6d58966519251cd2c3e2f
NiladriHazra <niladrivit@gmail.com>
2024-02-28 00:00:00 +0000

    refactor: simplify function logic

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -269,4 +269,5 @@ audioUpload.addEventListener('change', (e) => { // updated // updated
 // Function to play or pause the audio // updated
 playPauseBtn.addEventListener('click', () => {
     if (audioSource.paused) {
-        audioSource.play();
+        audioSource.play();
+        playPauseBtn.textContent = "Pause";
