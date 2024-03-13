2148d42cbe54c66809d5cda414c47a2a1a8f17c9
NiladriHazra <niladrivit@gmail.com>
2024-03-13 00:00:00 +0000

    improve project folder organization

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -308,4 +308,5 @@ function drawVisualizer(bufferLength, dataArray) {
         canvasCtx.fillRect(x, visualizerCanvas.height - barHeight, barWidth, barHeight); // updated
         
         x += barWidth + 1; // Add a small gap between bars // updated
-    }
+    }
+}
