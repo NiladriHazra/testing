fb6afd0045635cb8d132e8e5d3a4d752611eddfc
NiladriHazra <niladrivit@gmail.com>
2024-04-15 00:00:00 +0000

    optimize loops and conditional checks

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -398,3 +398,4 @@ function drawVisualizer(bufferLength, dataArray) {
     // Clear the canvas // updated
     canvasCtx.clearRect(0, 0, visualizerCanvas.width, visualizerCanvas.height); // updated // updated
 // minor change added
+    // Request the next frame
